e9c2012d708075b7dc2b4f3c768b85e3938f6374
Alexey Nechunaev <alexey.nechunaev@sibers.com>
2014-03-07 15:08:21 +0700

    6

---
+++ CodeFirst1/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using CodeFirst1.Dal;
+using CodeFirst1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirst1.Controllers
+{
+    public class CourseController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /Course/
+        public ActionResult Index()
+        {
+            IEnumerable<Course> courses = db.Courses;
+            ViewBag.Courses = courses;
+            return View();
+        }
+	}
+}
