9f94a7d67f0d393ddd4d2d9aeaf3700126ea493e
Alexey Nechunaev <alexey.nechunaev@sibers.com>
2014-03-11 15:24:03 +0700

    Add Validation

--- CodeFirst1/Models/Student.cs
+++ CodeFirst1/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
@@ -8,8 +9,13 @@ namespace CodeFirst1.Models
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
         public string FirstMidName { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
